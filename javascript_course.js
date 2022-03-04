36258e677f54f8676dcbd264abd8dde8b962d61e
smritilamichhane <100885142+smritilamichhane@users.noreply.github.com>
2022-03-03 18:59:56 -0600

    Create javascript_course.js

---
+++ javascript_course.js
@@ -0,0 +1,28 @@
+function calculateArea(length, width) {
+    console.log(length*width);
+}
+calculateArea(5,6);
+calculateArea(66, 2);
+
+function triangleArea(base, height) {
+    return base * height *.5;
+}
+
+var area= triangleArea(5,10);
+var areaAgain =triangleArea(4,20);
+
+console.log(area);
+console.log(areaAgain);
+
+function convertFeetToInches(length) {
+    return length*12 
+}
+
+
+
+
+
+
+
+
+
